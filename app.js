7d14539f6934d61db6c1cc318f7950653a94bf73
comanoana <oanamariarodila95@yahoo.es>
2021-03-18 18:16:28 +0200

    add some json changes

--- app.js
+++ app.js
@@ -28,6 +28,7 @@ app.use("/users", usersRouter);
 app.use("/teams", teamsDBRouter);
 app.use("/teams-json", teamsRouter);
 
+
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
   next(createError(404));
